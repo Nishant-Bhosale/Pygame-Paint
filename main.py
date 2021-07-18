be30f9c361c662cf1cfe7f2fce7b4c91fa788a53
Nishant <nishantbhosale244@gmail.com>
2021-07-18 13:25:55 +0530

    Fixed Interpretar error

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from utils import *
 
-WHITE
-BLACK
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Drawing Program')
+
+run = True
+
+while run:
+   for event in pygame.event.get():
+      if(event.type) == pygame.QUIT:
+         run = False
+
+pygame.quit()
