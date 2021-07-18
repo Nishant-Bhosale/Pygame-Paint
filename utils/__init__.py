b167aec06fc0bb384fc8bfa30563b2cf143c357c
Nishant <nishantbhosale244@gmail.com>
2021-07-18 12:49:36 +0530

    first commit

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from .settings import *
+import pygame
+pygame.init()
+pygame.font.init()
---
+++ utils/settings.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+WHITE = (255, 255,255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0,0,255)
+GREEN = (0, 255, 0)
+
+FPS = 60
+
+WIDTH, HEIGHT = 600, 700
+
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+ROWS = COLS - 50
+
+PIXEL_SIZE = WIDTH // ROWS
+
+BG_COLOR = WHITE
+
+DRAW_GRID_LINES = False
+
+def get_font(size):
+   return pygame.font.SysFont('comicsans', size)
